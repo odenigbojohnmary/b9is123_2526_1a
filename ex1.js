79e8f7fdcfc716a0cf97a8d4aa560d769eb93cc6
Johnmary Odenigbo <odenigbojohnmary@gmail.com>
2026-05-06 22:52:59 +0100

    updated the ex1 and ex2

--- ex1.js
+++ ex1.js
@@ -10,11 +10,13 @@
             //and let the user know when ok to proceed
 
             // Constants for business rules
-const MAX_WEIGHT = 15;
+const ALLOWED_WEIGHT = 15;
 let check = () => {
+  //This gets the value of the weight input from the input element identified by id='weight'
   let weightInput = document.getElementById('weight');
-  let weight = parseInt(weightInput.value);
-  
+  //This converts the value of the weight input to a float (used float to support decimals)
+  let weight = parseFloat(weightInput.value);
+  //This checks if the value of the weight input is not a number (not a valid number / or empty)
   if (isNaN(weight)) {
     alert('Please enter a valid weight.');
     return;
@@ -22,39 +24,51 @@ let check = () => {
   
   alert('Checking Baggage weight: ' + weight + 'kg');
   
-  if (weight > MAX_WEIGHT) {
-    alert('Baggage is overweight! Maximum allowed is' + MAX_WEIGHT + 'kg.');
+  //This checks if the weight is greater than the allowed weight (in our case 15kg)
+  if (weight > ALLOWED_WEIGHT) {
+    alert('Baggage is overweight! Maximum allowed is' + ALLOWED_WEIGHT + 'kg.');
+    //This makes the removed text input and button visible
     document.getElementById('removed').style.display = 'inline-block';
     document.getElementById('remB').style.display = 'inline-block';
   } else {
+    //This alerts the user that the baggage weight is ok
     alert('Baggage weight is OK. You may proceed.');
+    //This hides the removed text input and button
     document.getElementById('removed').style.display = 'none';
     document.getElementById('remB').style.display = 'none';
   }
 };
 
 let remove = () => {
+  //This gets the value of the weight input from the input element identified by id='weight'
   let weightInput = document.getElementById('weight');
+  //This gets the value of the removed input from the input element identified by id='removed'
   let removedInput = document.getElementById('removed');
-  
-  let currentWeight = parseInt(weightInput.value);
-  let amountToRemove = parseInt(removedInput.value);
-  
+  //This converts the value of the weight input to a float (used float to support decimals)
+  let currentWeight = parseFloat(weightInput.value);
+  //This converts the value of the removed input to a float (used float to support decimals)
+  let amountToRemove = parseFloat(removedInput.value);
+  //This checks if the amount to remove is not a number (not a valid number / or empty)
+  //or if the amount to remove is less than or equal to 0
   if (isNaN(amountToRemove) || amountToRemove <= 0) {
     alert('Please enter a valid amount to remove.');
     return;
   }
-  
+  //This subtracts the amount to remove from the current weight
   let newWeight = currentWeight - amountToRemove;
+  //This checks if the new weight is less than 0
   if (newWeight < 0) newWeight = 0;
-  
+  //This updates the weight input with the new weight
   weightInput.value = newWeight;
+  //This clears the removed input
   removedInput.value = '';
   
   alert('Removed ' + amountToRemove + 'kg. New weight is: ' + newWeight + 'kg');
   
-  if (newWeight <= MAX_WEIGHT) {
+  //This checks if the new weight is less than or equal to the allowed weight (in our case 15kg)
+  if (newWeight <= ALLOWED_WEIGHT) {
     alert('Baggage weight is now OK. You may proceed.');
+    //This hides the removed text input and button
     document.getElementById('removed').style.display = 'none';
     document.getElementById('remB').style.display = 'none';
   } else {
--- ex2.js
+++ ex2.js
@@ -2,10 +2,18 @@
 //https://projecteuler.net/problem=1
 //ex2 should output the sum of all multiples of a or b less than n
 
+
+// project Euler part 1 question
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
 let euler1=()=>{
+  //This initializes the sum to 0
   let sum = 0;
+  //This loops through numbers from 1 to 999
   for (let i = 1; i < 1000; i++) {
+    //This checks if the number is a multiple of 3 or 5
     if (i % 3 === 0 || i % 5 === 0) {
+      //This adds the number to the sum
       sum += i;
     }
   }
@@ -13,18 +21,24 @@ let euler1=()=>{
 };
 
 let eulerCustom=()=>{
+  //This gets the values of the input elements identified by id='a', id='b' and id='n'
   let a = parseInt(document.getElementById('a').value);
   let b = parseInt(document.getElementById('b').value);
   let n = parseInt(document.getElementById('n').value);
 
+  //This checks if the values are not numbers
   if (isNaN(a) || isNaN(b) || isNaN(n)) {
     alert("Please enter valid integers for A, B, and N.");
+    //This stops the function
     return;
   }
-
+  //This initializes the sum to 0
   let sum = 0;
+  //This loops through numbers from 1 to n-1
   for (let i = 1; i < n; i++) {
+    //This checks if the number is a multiple of a or b
     if (i % a === 0 || i % b === 0) {
+      //This adds the number to the sum
       sum += i;
     }
   }
